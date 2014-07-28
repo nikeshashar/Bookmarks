9ab924ce0fbbb2e255ed5e086444aacac45227bf
Nikesh Ashar <nikeshashar@gmail.com>
2014-07-28 13:35:35 +0100

    Updated Spec Helper

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,5 +1,18 @@
 require 'sinatra/base'
 
+env = ENV["RACK_ENV"] || "development"
+# we're telling the datamapper to use a postgres database on localhost. The name will be 
+# either bookmarks_test or bookmarks_development, depending on the environment
+DataMapper.setup(:default, "postgres://localhost/bookmarks_#{env}")
+
+require './lib/link' #this needs to be done after DataMapper has been initialised
+
+#After declaring your models you need to finalise them
+DataMapper.finalize
+
+#However the database tables don't exist yet. Let's tell Datamapper to create them
+DataMapper.auto_upgrade!
+
 class Bookmarks < Sinatra::Base
   get '/' do
     'Hello Bookmarks!'
