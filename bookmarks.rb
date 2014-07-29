de6f4ca3459c1f8104e284227287d06cfd206e59
Nikesh Ashar <nikeshashar@gmail.com>
2014-07-29 14:17:29 +0100

    Added signing in possibilty and RSpec = Green

--- bookmarks.rb
+++ bookmarks.rb
@@ -1,5 +1,8 @@
 require 'sinatra/base'
 require "data_mapper"
+require "bcrypt"
+
+
 
 env = ENV["RACK_ENV"] || "development"
 # we're telling the datamapper to use a postgres database on localhost. The name will be 
@@ -8,7 +11,7 @@ DataMapper.setup(:default, "postgres://localhost/bookmarks_#{env}")
 
 require './lib/link' #this needs to be done after DataMapper has been initialised
 require './lib/tag' #this needs to be done after DataMapper has been initialised
-
+require './lib/user' #this needs to be done after DataMapper has been initialised
 
 #After declaring your models you need to finalise them
 DataMapper.finalize
@@ -17,6 +20,10 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class Bookmarks < Sinatra::Base
+
+enable :sessions
+set :session_secret, 'super secret'
+
   get '/' do
   	@links = Link.all
  	erb :index
@@ -38,7 +45,22 @@ get '/tags/:text' do
 	erb :index
 end
 
+get '/users/new' do 
+  erb :"users/new"
+end
 
+post '/users' do 
+  user = User.create(:email => params[:email], :password => params[:password])
+  session[:user_id] = user.id
+  redirect to('/')
+end
+
+helpers do 
+
+  def current_user
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
+end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
